7259dc49baa0071bfe4ded28476ddcabe876b97f
vboxuser <vboxuser@debil.myguest.virtualbox.org>
2024-04-18 16:23:50 -0300

    terraformada

---
+++ terraform1/variables.tf
@@ -0,0 +1,20 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default = "udacity"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default = "East US"
+}
+
+variable "username" {
+  description = "The admin username for the VM being created."
+}
+
+variable "password" {
+  description = "The password for the VM being created."
+}

9a469010a13cc93be0cad352376d2eeb1c77a09c
vboxuser <vboxuser@debil.myguest.virtualbox.org>
2024-04-18 16:49:37 -0300

    fix subnet bug

--- terraform1/main.tf
+++ terraform1/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/25"]
+  address_prefixes     = ["10.0.0.0/28"]
 }
 resource "azurerm_public_ip" "main" {
   name                = "acceptanceTestPublicIp1"
